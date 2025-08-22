801fa71a0db07ee95b012a7c414ca9ffffe1d74e
Nisar k <nisardote@gmail.com>
2025-08-22 21:18:21 +0530

    Time: 16 ms (49.78%) | Memory: 61.2 MB (8.82%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -5,7 +5,17 @@
  */
 var isAnagram = function(s, t) {
     if (s.length !== t.length) return false;
-    return s.split('').sort().join('') === t.split('').sort()
-    .join('');
 
+    let map = {};
+
+    for (let char of s) {
+        map[char] = (map[char] || 0) + 1;
+    }
+
+    for (let char of t) {
+        if (!map[char]) return false;
+        map[char]--;
+    }
+
+    return true;
 };
