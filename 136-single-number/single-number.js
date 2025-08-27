52545ba171ce1e27655da3a25fc7d470152b7152
Nisar k <nisardote@gmail.com>
2025-08-27 12:30:20 +0530

    Time: 15 ms (17.98%) | Memory: 58.6 MB (23.47%) - LeetSync

---
+++ 136-single-number/single-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    nums.sort((a,b)=>a-b);
+    for(i=0;i<nums.length;i=i+2){
+        if(nums[i]!==nums[i+1]){
+            return nums[i];
+        }
+    }
+};
