3feff76992a7a3db8edbcceb947ab490782b8505
Nisar k <nisardote@gmail.com>
2025-09-02 19:53:26 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (42.87%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    if(nums.includes(target)){
+        return nums.indexOf(target);
+    }else{
+        for(let i=0;i<nums.length;i++){
+            if(nums[i]>target)return i;
+        }
+    }
+    return nums.length;
+};
