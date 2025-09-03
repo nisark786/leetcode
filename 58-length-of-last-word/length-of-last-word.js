68c28f3b6261a375cafbf2cb1b9cd0fffcc99c4d
Nisar k <nisardote@gmail.com>
2025-09-03 14:44:58 +0530

    Time: 0 ms (100.00%) | Memory: 53.1 MB (84.97%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -5,7 +5,7 @@
 var lengthOfLastWord = function(s) {
     let arr=s.split(" ");
     for(let i=arr.length-1;i>=0;i--){
-        if(arr[i]!=""){
+        if(arr[i]!==""){
             return arr[i].length;
         }
     }
