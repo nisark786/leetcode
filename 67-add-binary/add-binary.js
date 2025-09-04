472c2dd901ff674d943b93925448239587133922
Nisar k <nisardote@gmail.com>
2025-09-04 19:43:11 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (62.11%) - LeetSync

---
+++ 67-add-binary/add-binary.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+    let result = BigInt("0b"+a)+BigInt("0b"+b);
+    return result.toString(2);
+};
