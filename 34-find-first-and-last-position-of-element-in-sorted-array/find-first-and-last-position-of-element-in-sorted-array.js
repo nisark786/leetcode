5da75af8bcfea8fe93d10cd121a81efa22d73b80
Nisar k <nisardote@gmail.com>
2025-09-16 20:44:24 +0530

    Time: 0 ms (100.00%) | Memory: 55.6 MB (14.47%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,9 +4,46 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-    if(!nums.includes(target)){
-        return [-1,-1]
-    }else{
-        return [nums.indexOf(target),nums.lastIndexOf(target)]
+
+function findFirst(nums ,target){
+    let left = 0;
+    let right = nums.length-1;
+    let first = -1;
+    while(left<=right){
+        let mid = Math.floor((left+right)/2);
+        if(nums[mid]===target){
+            first = mid;
+            right = mid-1
+        }else if(nums[mid]<target){
+            left = mid+1;
+        }else{
+            right = mid-1;
+        }
     }
-};
+    return first
+
+}
+
+function findLast(nums ,target){
+    let left = 0;
+    let right = nums.length-1;
+    let last = -1;
+    while(left<=right){
+        let mid = Math.floor((left+right)/2);
+        if(nums[mid]===target){
+            last = mid;
+            left = mid+1
+        }else if(nums[mid]<target){
+            left = mid+1;
+        }else{
+            right = mid-1;
+        }
+    }
+    return last;
+
+}
+
+return [findFirst(nums,target),findLast(nums,target)]
+
+
+}
