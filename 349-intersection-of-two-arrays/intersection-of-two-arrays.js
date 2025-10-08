9fe5dec97cc620b2432e0c1d7d6bdc418883d724
Nisar k <nisardote@gmail.com>
2025-10-08 15:54:39 +0530

    Time: 2 ms (41.93%) | Memory: 55.6 MB (35.17%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -5,10 +5,8 @@
  */
 var intersection = function(nums1, nums2) {
     let result = [];
-    let iter = nums1.length>=nums2.length ? nums1 : nums2;
-    let not = nums1.length>=nums2.length ? nums2 : nums1;
-    for(let i of iter){
-        if(not.includes(i) && !result.includes(i)){
+    for(let i of nums1){
+        if(nums2.includes(i) && !result.includes(i)){
             result.push(i);
         }
     }
