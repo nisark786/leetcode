7aa0e5917f086a13208dac4c3887aa735a3787f9
Nisar k <nisardote@gmail.com>
2025-10-13 10:25:37 +0530

    Time: 18 ms (54.09%) | Memory: 77.2 MB (33.97%) - LeetSync

---
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    let set = new Set(nums)
+    let result = [];
+    for(let i=1;i<=nums.length;i++){
+        if(!set.has(i)){
+            result.push(i)
+        }
+    }
+    return result;
+};
